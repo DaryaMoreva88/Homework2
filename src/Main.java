7357d7fec7e71dc88050dd9cae64ca23822e1cef
moreva <belkastrelka88666@gmail.com>
2022-08-26 12:56:27 +0300

    коммит

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+public class Main {
+    public static void main(String[] args) {
+       double dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog +4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 3.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 3;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        System.out.println(firstBoxer);
+        System.out.println(secondBoxer);
+        var totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес боксеров " + totalWeight + " кг!");
+        var weightDifference = secondBoxer - firstBoxer;
+        System.out.println("Разница в весе боксеров составляет " + weightDifference + " кг!");
+        var remainderDivision  = secondBoxer % firstBoxer;
+        System.out.println("Разница в весе боксеров составляет " + remainderDivision + " кг!");
+        var workingHours = 640;
+        System.out.println(" В компании " + workingHours + "  рабочих часов");
+        var companyEmployees = workingHours / 8;
+        System.out.println(" Всего работников в компании -  " + companyEmployees + " человек");
+        var employeeGrowth = 94;
+        var totalEmployees = companyEmployees + employeeGrowth;
+        var totalHours = totalEmployees * 8;
+        System.out.println("Если в компании работает " + totalEmployees +  " человека, то всего " + totalHours  +  " часов работы может быть поделено между сотрудниками");
+    }
+}
